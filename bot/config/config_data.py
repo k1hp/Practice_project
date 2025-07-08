0bf1544aa2571acea07af9eaeaba4efdaec404ea
k1hp <provoda51@gmail.com>
2025-07-09 01:13:21 +0300

    изменены состояния и навигационные кнопки

---
+++ bot/config/config_data.py
@@ -0,0 +1,10 @@
+class CommonButtons:
+    navigation = {
+        "games": "🎮 Play",
+        "rating": "🏆 Rating",
+        "profile": "🐵 Profile and Balance",
+    }
+    games = {"saper": "💣 Saper", "red_black": "Red | Black"}
+
+
+START_BALANCE: int = 100
--- bot/core/states/common.py
+++ bot/core/states/common.py
@@ -3,5 +3,5 @@ from telebot.states import StatesGroup, State
 
 class UserState(StatesGroup):
     navigation = State()
-    echo = State()
-    date_time = State()
+    games = State()
+    profile = State()
