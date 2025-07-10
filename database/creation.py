3628d579f9b17f6af401d83ac4ca0fbf5f8ad830
k1hp <provoda51@gmail.com>
2025-07-10 18:00:46 +0300

    db nickname is not unique

--- database/creation.py
+++ database/creation.py
@@ -9,7 +9,7 @@ def create_tables(engine: Engine, logger) -> None:
         CREATE TABLE IF NOT EXISTS users (
             `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             `chat_id` INTEGER NOT NULL UNIQUE ,
-            `nickname` VARCHAR(100) NOT NULL UNIQUE,
+            `nickname` VARCHAR(100) NOT NULL,
             `balance` UNSIGNED BIG INT NOT NULL
         )
         """,
