d2207f3e1d2c8a23bd6fa06a699588e6e8e83e30
k1hp <provoda51@gmail.com>
2025-07-10 18:05:09 +0300

    add test_states

--- bot/core/states/common.py
+++ bot/core/states/common.py
@@ -5,3 +5,15 @@ class UserState(StatesGroup):
     navigation = State()
     games = State()
     profile = State()
+
+
+class GameState(StatesGroup):
+    black_jack = State()
+    saper = State()
+    test = State()
+
+
+class KMNState(StatesGroup):
+    balance = State()
+    game_process = State()
+    finish = State()  ## нужен или нет
