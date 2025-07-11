dca51a7752baa607c90103b3b63b11c0e7b5413d
k1hp <provoda51@gmail.com>
2025-07-12 01:00:15 +0300

    подредактированы состояния

--- bot/core/states/common.py
+++ bot/core/states/common.py
@@ -2,23 +2,39 @@ from telebot.states import StatesGroup, State
 
 
 class UserState(StatesGroup):
+    """
+    Состояния навигации.
+    """
     navigation = State()
     games = State()
     profile = State()
 
 
-class GameState(StatesGroup):
-    black_jack = State()
-    saper = State()
-    test = State()
-
-
 class KMNState(StatesGroup):
+    """
+    При наследовании от общего класса появляется непредсказуемость.
+    Состояния для игры Камни Ножницы Бумага
+    """
     balance = State()
     game_process = State()
     finish = State()  ## нужен или нет
 
 
 class RedBlackState(StatesGroup):
+    """
+    При наследовании от общего класса появляется непредсказуемость.
+    Состояния для игры Red | Black
+    """
+
+    balance = State()
+    game_process = State()
+
+
+class SaperState(StatesGroup):
+    """
+    При наследовании от общего класса появляется непредсказуемость.
+    Состояния для игры Saper
+    """
+
     balance = State()
     game_process = State()
